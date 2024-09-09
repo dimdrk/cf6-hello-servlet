6f5bbcc38dd8fd8ffdadc0e86a5d8eb760deced6
DimDrk <dimdrk@gmail.com>
2024-09-09 11:04:45 +0300

    line 26 change 'response' with 'request' error /inspect-request endpoint

--- src/main/java/gr/aueb/cf/helloworldservlet/controller/InspectRequestController.java
+++ src/main/java/gr/aueb/cf/helloworldservlet/controller/InspectRequestController.java
@@ -9,6 +9,7 @@ import java.util.Enumeration;
 
 @WebServlet("/inspect-request")
 public class InspectRequestController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -22,7 +23,7 @@ public class InspectRequestController extends HttpServlet {
         Enumeration<String> headerNames = request.getHeaderNames();
         while (headerNames.hasMoreElements()) {
             String headerName = headerNames.nextElement();
-            String headerValue = response.getHeader(headerName);
+            String headerValue = request.getHeader(headerName);
             System.out.println(headerName + ": " + headerValue);
             response.getWriter().write(headerName + ": " + headerValue + "\n");
         }
@@ -43,14 +44,14 @@ public class InspectRequestController extends HttpServlet {
                 // HttpSession session = request.getSession(false);     // if not session get null, else get session
         String jSessionId = session.getId();
         System.out.println("JSESSIONID: " + jSessionId);
-        response.getWriter().write("JSESSIONID: " + jSessionId);
+        response.getWriter().write("JSESSIONID: " + jSessionId + "\n");
 
 
         // Inspect session attributes
         request.getSession().setAttribute("username", username);
         String sessionUsername = (String) request.getSession().getAttribute("username");
-        System.out.println("Username: " + username);
-        response.getWriter().write("Username: " + username);
+        System.out.println("Username: " + sessionUsername);
+        response.getWriter().write("Username: " + sessionUsername + "\n");
 
         // Inspect request URI and context path
         System.out.println("Request URI: " + request.getRequestURI());
@@ -58,6 +59,6 @@ public class InspectRequestController extends HttpServlet {
         System.out.println("Servlet Path: " + request.getServletPath());
         response.getWriter().write("Request URI: " + request.getRequestURI() + "\n");
         response.getWriter().write("Context Path: " + request.getContextPath() + "\n");
-        response.getWriter().write("Servlet Path: " + request.getServletPath());
+        response.getWriter().write("Servlet Path: " + request.getServletPath() + "\n");
     }
 }
